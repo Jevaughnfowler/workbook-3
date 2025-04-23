90f08932ff79510dfecdee1b7b3d758592c47608
jevaughnfowler <jevaughnfowler@gmail.com>
2025-04-23 19:18:00 -0400

    first read file created

---
+++ web3demos/src/main/java/com/pluralsight/BufferedReaderDemo.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class BufferedReaderDemo {
+
+    public static void main(String[] args)  {
+
+        FileReader fr = null;
+        try {
+            fr = new FileReader("jokes.txt");
+
+            BufferedReader br = new BufferedReader(fr);
+
+            String currentLine;
+
+            while((currentLine = br.readLine()) != null){
+                System.out.println(currentLine);
+            }
+
+            br.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+}
